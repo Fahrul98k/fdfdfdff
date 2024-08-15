f34384b080e06735c774bd914ed2587039617c67
Fahrul98k <91683690+Fahrul98k@users.noreply.github.com>
2024-08-15 20:49:08 +0800

    Update FlowerGiphy.js

--- lase/src/component/FlowerGiphy.js
+++ lase/src/component/FlowerGiphy.js
@@ -3,17 +3,18 @@ import React from 'react';
 
 const FlowerGiphy = ({ type }) => {
     // URL GIF dari Giphy
-    const giphyUrl = "https://giphy.com/embed/qCH3Pqkc1T6NAJtTZs/video"; // Ganti dengan URL GIF sesuai kebutuhan
+    const giphyUrl = "https://giphy.com/embed/HIh3Bjd9edbvzPnOYg"; // Ganti dengan URL GIF sesuai kebutuhan
 
     return (
-        <div>
-            <iframe
-                allow="fullscreen"
-                frameBorder="0"
-                height="360"
-                src={giphyUrl}
-                width="480"
-                title="Giphy"
+        <div style={{ textAlign: 'center' }}>
+            <iframe 
+                src={giphyUrl} 
+                width="320" 
+                height="320" 
+                frameBorder="0" 
+                className="giphy-embed" 
+                allowFullScreen
+                title="Giphy GIF"
             ></iframe>
         </div>
     );
